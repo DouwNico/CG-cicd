9c3366305276e117a93ef34f487cc8e81c892846
DouwNico <douwe.nicolaas@gmail.com>
2022-01-16 16:17:02 +0100

    fix: spacing

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -1,9 +1,11 @@
 from buzz import generator
 
+
 def test_sample_single_word():
     test1 = ('foo', 'bar', 'foobar')
     word = generator.sample(test1)
     assert word in test1
+
 
 def test_sample_multiple_words():
     test2 = ('foo', 'bar', 'foobar')
@@ -12,6 +14,7 @@ def test_sample_multiple_words():
     assert words[0] in test2
     assert words[1] in test2
     assert words[0] is not words[1]
+
 
 def test_generate_buzz_of_at_least_five_words():
     phrase = generator.generate_buzz()

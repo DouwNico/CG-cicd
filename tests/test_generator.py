2eb5b519022c6d46cf5ae7724c92a57e3b50a89c
DouwNico <douwe.nicolaas@gmail.com>
2022-01-16 16:11:59 +0100

    fix: fixed lint errors

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -1,18 +1,16 @@
-import unittest
-
 from buzz import generator
 
 def test_sample_single_word():
-    l = ('foo', 'bar', 'foobar')
-    word = generator.sample(l)
-    assert word in l
+    test1 = ('foo', 'bar', 'foobar')
+    word = generator.sample(test1)
+    assert word in test1
 
 def test_sample_multiple_words():
-    l = ('foo', 'bar', 'foobar')
-    words = generator.sample(l, 2)
+    test2 = ('foo', 'bar', 'foobar')
+    words = generator.sample(test2, 2)
     assert len(words) == 2
-    assert words[0] in l
-    assert words[1] in l
+    assert words[0] in test2
+    assert words[1] in test2
     assert words[0] is not words[1]
 
 def test_generate_buzz_of_at_least_five_words():

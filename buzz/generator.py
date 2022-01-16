89246a99dad8c885af643b819dc2b3892bf154d5
DouwNico <douwe.nicolaas@gmail.com>
2022-01-16 16:19:23 +0100

    fix: spacing v2

--- buzz/generator.py
+++ buzz/generator.py
@@ -8,7 +8,7 @@ adverbs = ('remarkably', 'enormously', 'substantially', 'significantly', 'seriou
 verbs = ('accelerates', 'improves', 'enhances', 'revamps', 'boosts')
 
 
-def sample(word, n = 1):
+def sample(word, n=1):
     result = random.sample(word, n)
     if n == 1:
         return result[0]
@@ -17,8 +17,7 @@ def sample(word, n = 1):
 
 def generate_buzz():
     buzz_terms = sample(buzz, 2)
-    phrase = ' '.join([sample(adjectives), buzz_terms[0], sample(adverbs),
-        sample(verbs), buzz_terms[1]])
+    phrase = ' '.join([sample(adjectives), buzz_terms[0], sample(adverbs), sample(verbs), buzz_terms[1]])
     return phrase.title()
 
 
